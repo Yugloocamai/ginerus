ab7c45013afe157986315ecfda99c3254a2ce314
Yugloocamai <Yugloocamai@gmail.com>
2018-01-29 13:10:51 -0800

    Update ginerus.go
    Added parsing of config options format and output

--- ginerus.go
+++ ginerus.go
@@ -12,7 +12,24 @@ func Ginerus() gin.HandlerFunc {
 	return GinerusWithLogger(logrus.StandardLogger())
 }
 
-func GinerusWithLogger(logger *logrus.Logger) gin.HandlerFunc {
+func GinerusWithLogger(logger *logrus.Logger, config map[string]string) gin.HandlerFunc {
+	
+	// parse config
+	if _, ok := config["format"]; ok {
+		if config["format"] == "json" {
+			// Log as JSON instead of the default ASCII formatter.
+			logrus.SetFormatter(&logrus.JSONFormatter{})
+		}
+	}
+	if _, ok := config["output"]; ok {
+		if config["output"] == "stdout" {
+			logrus.SetOutput(os.Stdout)
+		}
+		if config["output"] == "stderr" {
+			logrus.SetOutput(os.Stderr)
+		}
+	}
+	
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
